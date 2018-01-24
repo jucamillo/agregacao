db89b065e0085edc9db10544d3f2874323a3679c
php 04 <php.04@xuxu.cec.ime.usp.br>
2018-01-24 21:48:18 -0200

    Teste de construtor

--- aluno.php
+++ aluno.php
@@ -1,6 +1,5 @@
 <?php
-
-
+declare(strict_types = 1);
       require 'lib/Carbon.php';
       use Carbon\Carbon;
 
@@ -10,10 +9,19 @@ class aluno
   public $dataDeNascimento; //atributo
   public $rg; //atributo
   public $turma; //atributo
+
+  //funcao que é chamada sempre que um aluno é criada (construtor)
+
+  //se coloca um parametro dentro de um construtor esse parametro se torna 'obrigatorio', ele nao deixa de criar, ele apenas da um aviso
+// so da erro se pedir o tipo, p pedir o tipo tem que add o declare la em cima
+  function __construct(string $nome, Carbon $dataDeNascimento)
+  {
+    $this->nome = $nome;
+    $this->dataDeNascimento = $dataDeNascimento;
+    echo "Aluno Criado" . PHP_EOL;
+  }
 }
 
 
-$aluno = new aluno();
-$aluno->nome = "Lorem Ipsum";
-$aluno->dataDeNascimento = Carbon::createFromDate(1940, 5, 21, 'America/Sao_Paulo');
+$aluno = new aluno("Lorem", Carbon::createFromDate(1940, 5, 21, 'America/Sao_Paulo'));
 $aluno->rg = 333333333;
